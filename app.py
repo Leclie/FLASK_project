134e2ec8d2b79c5fb8fc1813e3b91a06f63df0f2
Leclie <07140714no@gmail.com>
2024-01-17 16:16:43 +0300

    HW_2 commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, make_response
 
 app = Flask(__name__)
 
@@ -26,6 +26,41 @@ def product(product_id):
     # Для примера, возвращаем просто словарь с данными о товаре
     product_data = {'id': product_id, 'name': 'Пример товара', 'price': 50, 'description': 'Описание товара'}
     return render_template('product.html', product=product_data)
+
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+
+        # Создание cookie
+        response = make_response(redirect(url_for('welcome')))
+        response.set_cookie('user_data', f'{name},{email}')
+
+        return response
+
+    return render_template('login.html')
+
+@app.route('/welcome')
+def welcome():
+    user_cookie = request.cookies.get('user_data')
+    if user_cookie:
+        name, _ = user_cookie.split(',')
+        return render_template('welcome.html', name=name)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    # Удаление cookie
+    response = make_response(redirect(url_for('login')))
+    response.delete_cookie('user_data')
+    return response
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
